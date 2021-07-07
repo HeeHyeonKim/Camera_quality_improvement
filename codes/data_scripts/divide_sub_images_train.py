a668a4db17df22a0fd01025e0d8777b1dcafa506
HEEHYEON <55612326+HeeHyeonKim@users.noreply.github.com>
2021-07-07 17:08:28 +0900

    visualize.ipynb를 분석한 결과에 맞추어 threshold를 기준으로 sub-img를 분할한다.
    visualize.ipynb를 분석한 결과에 맞추어 threshold를 기준으로 sub-img를 3개의 집합으로 분할한다. 이는 PSNR 분포의 사분위수를 이용한다. 첫 번째 기준점은 25%가 되는 지점인 20.809031, 두 번째 기준점은 75%가 되는 지점인 32.972153으로 한다.

--- codes/data_scripts/divide_sub_images_train.py
+++ codes/data_scripts/divide_sub_images_train.py
@@ -3,18 +3,19 @@ import os.path as osp
 import os
 import numpy as np
 import shutil
+from tqdm import tqdm
 
 
 #divide training data
-LR_folder= './datasets/train_label_img'
-GT_folder= './datasets/train_input_img'
+LR_folder= '.\\datasets\\train_LR_sub_img'
+GT_folder= '.\\datasets\\train_GT_sub_img'
 
-save_list=["./datasets/scale_sub_psnr_LR_class3",
-           "./datasets/scale_sub_psnr_LR_class2",
-           "./datasets/scale_sub_psnr_LR_class1",
-           "./datasets/scale_sub_psnr_GT_class3",
-           "./datasets/scale_sub_psnr_GT_class2",
-           "./datasets/scale_sub_psnr_GT_class1"]
+save_list=[".\\datasets\\SR_branches_datasets\\scale_sub_psnr_LR_class3", # Hard
+           ".\\datasets\\SR_branches_datasets\\scale_sub_psnr_LR_class2", # Middle
+           ".\\datasets\\SR_branches_datasets\\scale_sub_psnr_LR_class1", # Easy
+           ".\\datasets\\SR_branches_datasets\\scale_sub_psnr_GT_class3",
+           ".\\datasets\\SR_branches_datasets\\scale_sub_psnr_GT_class2",
+           ".\\datasets\\SR_branches_datasets\\scale_sub_psnr_GT_class1"]
            
 for i in save_list:
     if os.path.exists(i):
@@ -22,37 +23,33 @@ for i in save_list:
     else:
         os.makedirs(i)
 
-threshold=[27.16882,35.149761]
+threshold=[20.809031,32.972153]
 
-f1 = open("./codes/data_scripts/divide_train.log")
-a1 = f1.readlines()
-
-txt_file = open("./datasets/PSNR_log.txt", 'w')
+txt_file = open("./PSNR_logs.txt", 'r')
+a1 = txt_file.readlines()
 
 index=0
-for i in a1:
+for i in tqdm(a1):
     # i = 1 line
     index+=1
-    print(index)
+    # print(index)
 
-    if ('- PSNR:' in i and 'INFO:' in i) and ('results' not in i):
-        psnr=float(i.split('PSNR: ')[1].split(' dB')[0])
-        filename=i.split('INFO: ')[1].split(' ')[0]
-        filename=filename+".png"
-        print(filename,psnr)
-        log = str(filename) + ", " + str(psnr) + '\n'
-        txt_file.write(log)
+    psnr=float(i.split(" : ")[1])
+    filename=i.split(",")[0] # GT
+    new_filename = filename.split("_")[0] + "_input_" + filename.split("_")[2] + "_" + filename.split("_")[3] # LR
+    # print(filename," ",new_filename," ",psnr)
 
-        # if psnr < threshold[0]:
-        #     shutil.copy(osp.join(LR_folder, filename), osp.join(save_list[0], filename))
-        #     shutil.copy(osp.join(GT_folder, filename), osp.join(save_list[3], filename))
-        # if psnr >= threshold[0] and psnr < threshold[1]:
-        #     shutil.copy(osp.join(LR_folder, filename), osp.join(save_list[1], filename))
-        #     shutil.copy(osp.join(GT_folder, filename), osp.join(save_list[4], filename))
-        # if psnr >= threshold[1]:
-        #     shutil.copy(osp.join(LR_folder, filename), osp.join(save_list[2], filename))
-        #     shutil.copy(osp.join(GT_folder, filename), osp.join(save_list[5], filename))
+    if psnr < threshold[0]:
+        # Hard
+        shutil.copy(osp.join(LR_folder, new_filename), osp.join(save_list[0], new_filename))
+        shutil.copy(osp.join(GT_folder, filename), osp.join(save_list[3], filename))
+    if psnr >= threshold[0] and psnr < threshold[1]:
+        # Middle
+        shutil.copy(osp.join(LR_folder, new_filename), osp.join(save_list[1], new_filename))
+        shutil.copy(osp.join(GT_folder, filename), osp.join(save_list[4], filename))
+    if psnr >= threshold[1]:
+        # Easy
+        shutil.copy(osp.join(LR_folder, new_filename), osp.join(save_list[2], new_filename))
+        shutil.copy(osp.join(GT_folder, filename), osp.join(save_list[5], filename))
 
 txt_file.close()
-f1.close()
-
